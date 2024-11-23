37be377ed97db54ea398f0c840457fbfff11abc1
AnnaAksenova444 <samoilenko.anny@yandex.ru>
2024-11-23 23:30:43 +0300

    navigation-controller-segue

---
+++ ImageLike/ProfileViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginLabel: UILabel!
+    @IBOutlet var characteristicLabel: UILabel!
+    @IBOutlet var exitButton: UIButton!
+    
+    @IBAction func didTapExitButton() {
+    }
+}
