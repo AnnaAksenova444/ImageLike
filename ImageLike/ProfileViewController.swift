57577d4d408946a7db97d48d2d61f1d103548a01
AnnaAksenova444 <samoilenko.anny@yandex.ru>
2024-11-30 03:21:43 +0300

    Выполнено задание 9-ого спринта

--- ImageLike/ProfileViewController.swift
+++ ImageLike/ProfileViewController.swift
@@ -2,12 +2,79 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet var photoImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var loginLabel: UILabel!
-    @IBOutlet var characteristicLabel: UILabel!
-    @IBOutlet var exitButton: UIButton!
+    private var photoImageView = UIImageView()
+    private var nameLabel = UILabel()
+    private var loginLabel = UILabel()
+    private var characteristicLabel = UILabel()
+    private var exitButton = UIButton()
     
-    @IBAction func didTapExitButton() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createAvatar()
+        createLabels()
+        createButton()
     }
+    
+    private func createAvatar () {
+        photoImageView.image = UIImage.photo
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(photoImageView)
+        
+        NSLayoutConstraint.activate([
+            photoImageView.heightAnchor.constraint(equalToConstant: 70),
+            photoImageView.widthAnchor.constraint(equalToConstant: 70),
+            photoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            photoImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    private func createLabels() {
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.textColor = .ypWhiteAlpha50
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        
+        characteristicLabel.text = "Hello, world!"
+        characteristicLabel.textColor = .ypWhite
+        characteristicLabel.font = UIFont.systemFont(ofSize: 13)
+        characteristicLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(characteristicLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 8),
+            nameLabel.leftAnchor.constraint(equalTo: photoImageView.leftAnchor),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginLabel.leftAnchor.constraint(equalTo: nameLabel.leftAnchor),
+            characteristicLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            characteristicLabel.leftAnchor.constraint(equalTo: loginLabel.leftAnchor)
+        ])
+    }
+    
+    private func createButton() {
+        exitButton = UIButton.systemButton(
+            with: UIImage.exit,
+            target: self,
+            action: #selector(Self.didTapLogoutButton))
+        exitButton.tintColor = .ypRed
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+        
+        NSLayoutConstraint.activate([
+            exitButton.centerYAnchor.constraint(equalTo: photoImageView.centerYAnchor),
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            exitButton.heightAnchor.constraint(equalToConstant: 44),
+            exitButton.widthAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    @objc
+    private func didTapLogoutButton() {}
 }
