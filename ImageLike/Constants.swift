a1b756520d4fa8230f75398d5bfd93f34930380b
AnnaAksenova444 <samoilenko.anny@yandex.ru>
2024-12-24 20:28:40 +0300

    Исправлен файл Constants

--- ImageLike/Constants.swift
+++ ImageLike/Constants.swift
@@ -3,9 +3,9 @@ import UIKit
 enum Constants {
     static let accessKey = "0A_CUBkk2QekwpkN6BpgdCZ_I5TJIFFmvlOwKC5JyDI"
     static let secretKey = "9F1w-hUiNXYU1F7N1WRGQcRQjCagF0dSw9mjaQjgRAY"
-    static let redirectURI = "682668"
-    static let accessScope = "Public access + Read user access + Write likes access"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
 }
 
 
